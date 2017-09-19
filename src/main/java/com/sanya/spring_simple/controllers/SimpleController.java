6e5d1eb496f1aa6b620c1eb7949795c1f48b3a6c
Alexander <zemsanya@gmail.com>
2017-09-19 04:45:59 +0300

    all

---
+++ src/main/java/com/sanya/spring_simple/controllers/SimpleController.java
@@ -0,0 +1,37 @@
+package com.sanya.spring_simple.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Sanle on 9/15/2017.
+ */
+@RestController
+public class SimpleController {
+    @RequestMapping("/")
+    public String index(HttpServletRequest request) {
+        StringBuilder userInfoSb = new StringBuilder();
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        userInfoSb.append("Local address: ").append(request.getRemoteAddr()).append(System.getProperty("line.separator"))
+                .append("Forwarded for:").append(ip);
+        return userInfoSb.toString();
+    }
+}
