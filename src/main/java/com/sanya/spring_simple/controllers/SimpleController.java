d6c1654600e1f6915d2babe8d0c78e5a39b57147
Alexander <zemsanya@gmail.com>
2017-09-19 05:06:05 +0300

    changed output

--- src/main/java/com/sanya/spring_simple/controllers/SimpleController.java
+++ src/main/java/com/sanya/spring_simple/controllers/SimpleController.java
@@ -15,23 +15,38 @@ public class SimpleController {
     public String index(HttpServletRequest request) {
         StringBuilder userInfoSb = new StringBuilder();
         String ip = request.getHeader("X-Forwarded-For");
+        if (ip != null) {
+            userInfoSb.append("X-Forwarded-For: ").append(ip).append("\n");
+        }
+
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
+            if (ip != null) {
+                userInfoSb.append("Proxy-Client-IP: ").append(ip).append("\n");
+            }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
+            if (ip != null) {
+                userInfoSb.append("WL-Proxy-Client-IP: ").append(ip).append("\n");
+            }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
+            if (ip != null) {
+                userInfoSb.append("HTTP_CLIENT_IP: ").append(ip).append("\n");
+            }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            if (ip != null) {
+                userInfoSb.append("HTTP_X_FORWARDED_FOR: ").append(ip).append("\n");
+            }
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        userInfoSb.append("Local address: ").append(request.getRemoteAddr()).append(System.getProperty("line.separator"))
-                .append("Forwarded for:").append(ip);
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getRemoteAddr();
+//        }
+        userInfoSb.append("Local address: ").append(request.getRemoteAddr());
         return userInfoSb.toString();
     }
 }
