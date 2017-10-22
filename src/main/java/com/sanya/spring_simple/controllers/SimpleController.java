c04f0c35e9ca99401978861fd5716737a28a62f4
Alexander <zemsanya@gmail.com>
2017-10-22 16:57:11 +0300

    some changes

--- src/main/java/com/sanya/spring_simple/controllers/SimpleController.java
+++ src/main/java/com/sanya/spring_simple/controllers/SimpleController.java
@@ -11,42 +11,43 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 public class SimpleController {
+
     @RequestMapping("/")
     public String index(HttpServletRequest request) {
         StringBuilder userInfoSb = new StringBuilder();
+        userInfoSb.append("Local address: ").append(request.getRemoteAddr()).append("; ");
         String ip = request.getHeader("X-Forwarded-For");
         if (ip != null) {
-            userInfoSb.append("X-Forwarded-For: ").append(ip).append("\n");
+            userInfoSb.append("X-Forwarded-For: ").append(ip).append("; ");
         }
 
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
             if (ip != null) {
-                userInfoSb.append("Proxy-Client-IP: ").append(ip).append("\n");
+                userInfoSb.append("Proxy-Client-IP: ").append(ip).append("; ");
             }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
             if (ip != null) {
-                userInfoSb.append("WL-Proxy-Client-IP: ").append(ip).append("\n");
+                userInfoSb.append("WL-Proxy-Client-IP: ").append(ip).append("; ");
             }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
             if (ip != null) {
-                userInfoSb.append("HTTP_CLIENT_IP: ").append(ip).append("\n");
+                userInfoSb.append("HTTP_CLIENT_IP: ").append(ip).append("; ");
             }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
             if (ip != null) {
-                userInfoSb.append("HTTP_X_FORWARDED_FOR: ").append(ip).append("\n");
+                userInfoSb.append("HTTP_X_FORWARDED_FOR: ").append(ip).append("; ");
             }
         }
 //        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 //            ip = request.getRemoteAddr();
 //        }
-        userInfoSb.append("Local address: ").append(request.getRemoteAddr());
         return userInfoSb.toString();
     }
 }
